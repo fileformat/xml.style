9935e34d10483cbd548b375a30511505b39f65bf
Andrew Marcuse <fileformat@gmail.com>
2025-08-24 14:50:51 -0400

    Removed leftover stuff from rss.style

--- build.sh
+++ build.sh
@@ -9,17 +9,6 @@ echo "INFO: build starting at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
 echo "INFO: Jekyll location = $(which jekyll)"
 echo "INFO: Jekyll version = $(jekyll --version)"
 
-# check if minify is installed
-MINIFY="$(go env GOPATH)/bin/minify"
-if [ ! -f "${MINIFY}" ];
-then
-    echo "INFO: minify could not be found, installing"
-    go install github.com/tdewolff/minify/cmd/minify@latest
-fi
-
-cat docs/xslt/simple-rss.xslt | "${MINIFY}" --type=xml | base64 > docs/xslt/simple-rss.base64
-cat docs/xslt/simple-atom.xslt | "${MINIFY}" --type=xml | base64 > docs/xslt/simple-atom.base64
-
 jekyll build --source docs
 
 echo "INFO: build complete at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
