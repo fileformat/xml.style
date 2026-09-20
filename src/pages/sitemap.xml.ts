41445dcbe5bbf5791800e1de9ef9e187c6b7bb89
Andrew Marcuse <fileformat@gmail.com>
2026-09-20 14:28:09 -0400

    Add docs for using custom sitemap schema (and use it locally)

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,19 +2,19 @@ import type { APIRoute } from 'astro';
 import { productionUrl, sitemapPageUrls } from '../data/site';
 
 export const GET: APIRoute = () => {
-  const body = `<?xml version="1.0" encoding="UTF-8"?>
+	const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9"
-  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <script src="/sitemap/basic.js" xmlns="http://www.w3.org/1999/xhtml"></script>
-    ${sitemapPageUrls.map((url) => `  <url><loc>${productionUrl}${url}</loc></url>`).join("\n")}
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.xml.style/schemas/sitemap/0.9/sitemap.xsd"
+	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	<script src="/sitemap/basic.js" xmlns="http://www.w3.org/1999/xhtml"></script>
+	${sitemapPageUrls.map((url) => `\t<url>\n\t\t<loc>${productionUrl}${url}</loc>\n\t</url>`).join("\n")}
 </urlset>
 `;
 
-  return new Response(body, {
-    headers: {
-      'Content-Type': 'application/xml; charset=utf-8'
-    }
-  });
+	return new Response(body, {
+		headers: {
+			'Content-Type': 'application/xml; charset=utf-8'
+		}
+	});
 };
